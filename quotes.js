9d09d08d64940cf5e2bad623787bbd03d5795535
Pedro Figuera <pedroafiguera@gmail.com>
2013-12-29 20:21:10 -0500

    first commit quotes project

---
+++ quotes.js
@@ -0,0 +1,239 @@
+var quotes	= ["Stand up boy, stand up and fight.",
+
+	"The competitor who wont go away, who won't stay down, has one of the most formidable competitive advantages of all. P11",
+
+	"Plans move you forward.",
+
+	"Winners act like winners, before they are winners.",
+
+	"Consistent effort is a consistent challenge.",
+
+	"He hired coaches with the expectations of them moving on.",
+
+	"Opportunity is in the eye of the beholder.",
+
+	"Is it right there in front of you, unseen because your thinking is rigid and resistant to originality and change?",
+
+	"My five do's for getting back into the game: 1. Do expect defeat- if you are surprised when it happens, you're dreaming; dreamers don't last long. 2. Do force yourself to stop looking backward and dwelling on the professional train wreck you have just been in. Its mental quick sand. 3. Do allow yourself appropriate recovery-grieving-time. 4. Do tell yourself, I am going to stand and fight again, with the knowledge that often when things are at their worst you are closer than you can imagine to success. 5. Do begin planning for your next serious encounter",
+
+	"Lessons of the offense: 1.Success doesn't care which road you take to get to its doorstep. 2. Remove fear of the unknown-that is, change- from your mind. 3. Desperation should not drive innovation. 4.	Be obsessive in looking for the upside in the downside.",
+
+	"Few things offer greater return on less investment than praise. P48",
+
+	"Contingency planning is critical",
+
+	"Under stress you are not as good.  Unless, that is, you've planned and thought through the steps you are going to take in all situations- your contingency plan. P51",
+
+	"Everything has to be strategized. P53",
+
+	"In business, every day is game day.",
+
+	"Progress can be measured in a variety of ways.",
+
+	"Where do you want to go? How to get there? Then move forward with determination. P78",
+
+	"I will not quit",
+
+	"It is good to remind yourself that this quality, strength of will, is essential in your survival and success.P78-79",
+
+	"The difference between offering an opinion and making a decision, is the difference between between working for the leader and being the leader. P79",
+
+	"What is the plan of action?",
+
+	"Checklist when it comes to persevering: P83 1.	A leader must never quit. 2. A leader must know when to quit. 3. Proving that you are right or proving that someone is wrong are bad reasons for persisting. 4.	Good logic, sound principles, and strong belief are the purest and most productive reasons for pushing forward when things get rough.",
+
+	"Be a leader: List P84 1. Be yourself. 2.	Be committed to excellence. 3. Be positive. 4. Be prepared (good luck is a product of good planning) 5. Be detail oriented. 6. Be organized. 7. Be accountable. 8.	Be near sight and far sighted. 9.	Be fair. 10. Be firm. 11. Be flexible. 12. Believe in yourself. 13. Be a leader.",
+
+	"If you are faking it, you'll be found out.",
+
+ 	"Plan and prepare for the unexpected.",
+
+ 	"Excuse making is contagious.",
+
+ 	"You must know where you are going and how you intend to get there.",
+
+ 	"Don't distract yourself from the tough road ahead. P88",
+
+ 	"Inner voice talking to a leader: P100 1.	We can win if we work smart enough and hard enough. 2. We can win if we put the good of the group ahead of our own personal interests. 3.	We can win if we improve. And there is always room for improvement.",
+
+	"Lead by example.",
+
+ 	"Command list: P105 1. Remember that praise is more valuable than blame. 2. Use every means before and after combat to tell troops what they are going to do and what they have done. 3. Discipline is based on pride in the proffesion, on meticulous attention to details and on mutual respect and confidence. 4. Officers must assert themselves by example and by voice. 5. General officers must be seen in the front line during action.",
+
+	"Don't mistake activity for achievement. P111",
+
+ 	"The skills of being a great listener is the first law of communication.",
+
+ 	"The second is when you are not listening ask good questions.",
+
+ 	"If our ship sank, we all drowned.",
+
+ 	"One of the greatest and most neglected skills in leadership is the ability to listen. P117",
+
+ 	"Listen, learn, lead.",
+
+ 	"Don't fall into a mental comfort zone.",
+
+ 	"In a highly competitive environment, feeling confortable is the first cousin to being complacent. P119",
+
+ 	"We stuck to the plan.",
+
+ 	"It is not a duty or burden that you get out of your way so you can move on to important things.  It is the important thing. P123",
+
+ 	"The more you know, the higher you go. P123",
+
+ 	"Only expertise makes you an expert.",
+
+ 	"I came to understand that if the audience is bored, it is not their fault. And when they are plugged in and excited, its because of you, the person in stage.",
+
+ 	"If you don't love it, don't do it. P129",
+
+ 	"Bill liked order.",
+
+ 	"Use the time following success to make hard decisions. P146",
+
+ 	"Lack of the stick with it attitude is accompanied by a certain lack of intelligence. P150",
+
+ 	"Aspiring isn't enough. You must also have the strength of commitment and sacrifice to adhere to those standards and ideals in both good times and bad. P150",
+
+ 	"Commitment and sacrifice are among the personal characteristics I value most highly in people.",
+
+ 	"What a big ego is: pride, self-confidence, self assurance, and self-esteem.",
+
+ 	"Death spiral.",
+
+ 	"Your competitor must never look at you and say I not only beat you, I broke your spirit.",
+
+ 	"A good leader must exercise extreme prudence.",
+
+ 	"You can't transplant will power. P165",
+
+ 	"Occasionally, when striving to go beyond conventional results, you must go beyond the conventional and against the popular opinion. This means trusting your own judgment enough to be resourceful, innovative, and imaginative. It means resisting the herd mentality. P167",
+
+ 	"Conventional wisdom often produces conventional results.",
+
+ 	"Conventional wisdom didn't produce Jerry Rice.",
+
+ 	"Keep your eye in the ball list: P173 1. Concentrate on what will produce results rather than on the results, the process rather than the prize. 2.	Exhibit an inner toughness emanating from four of the most effective survival tools a leader can possess: expertise, composure, patience, and common sense. 3. Maintain your level of professional ethics and all details of your own Standard of Performance. 4.	Don't isolate yourself. 5. Don't let the magnitude of the challenge take you away from the incremental steps necessary to effect change. 6.	Exude an upbeat and determined attitude. 7.	Hold meetings with staff educating them on what to expect. 8.	Don't label some concept or new plan the thing that will “get us back on track”. 9.	Ensure that an appropriate level of courtesy and respect is extended to all members of the organization. 10. Don't plead with employees to “do better”. 11. Avoid continual threatening or chastising. 12. Deal with your immediate superior(s) on a one-to-one, ongoing basis.",
+
+ 	"Make your own method.",
+
+ 	"A good leader is always learning. P177",
+
+ 	"The difference is how much are you willing to give to get there.",
+
+ 	"There was no single individual in the organization who could say he/she outworked me.",
+
+ 	"Connection between hard work and survival, between survival and success.",
+
+ 	"I had a passion for trying to determine how we could have performed at a higher level, how we could achieve perfection, or at least get closer  and closer and closer. P189",
+
+ 	"My strength? I have a passion for what I do.",
+
+ 	"Don't get all worked up and stressed out by everything. Stay loose. P205",
+
+ 	"Those who are able to perform best are those who are best able to remove tension, anxiety and fear from their minds. P206",
+
+ 	"Criticism is part of the territory when you're the one calling the shots.",
+
+ 	"Concern yourself only with what you can control. P210",
+
+ 	"Famous last words: I'll do it myself",
+
+ 	"When you make a mistake, admit it and fix it. P215",
+
+ 	"You are putting yourself on a slippery slope when you start believing that the outcome of your effort represents or embodies who you really are as a person-what your value as a person is. P219",
+
+ 	"There are times when you must stand up for yourself, even if the consequences include being fired. P221",
+
+ 	"List P225: 1. Do not isolate yourself. 2. Delegate abundantly. 3.	Avoid the destructive temptation to define yourself as a person by the won-lost record, the score. 4.	You only let it bother you for 24 hours and then its over.",
+
+ 	"One of the most common traits of outstanding performers is that rejection, that is, defeat is quickly forgotten, replaced eagerly by pursuit of a new order, client, or opponent. They know that a defeat, can't be taken personally. They shake it off and go forward. And so must you. P226",
+
+ 	"Nothing is more gratifying than creating something that you are sure no one else has ever seen or thought of and having it succeed.",
+
+ 	"Reliable results take time.",
+
+ 	"Today's efforts become tomorrow's results. The quality of those efforts becomes the quality of your work. P231"
+];
+
+var selectedQuotes = []; 
+
+function printQuote(n){
+	$('#quote').html(quotes[n]);
+}
+
+function selectedQuote(n){
+	var search = $.inArray(quotes[n],selectedQuotes);
+
+	if (search >= 0 ){
+		alert("quote was already selected");
+	} else {
+		selectedQuotes.push(quotes[n]);
+		$('#selected').append("<li>" + selectedQuotes[selectedQuotes.length-1] + "</li>");
+	}
+}
+
+function unselectQuote(n){
+// 	if selectedQuotes.length > 0{
+	if (selectedQuotes.length > 1) {
+		selectedQuotes.pop();
+		$('#selected').html("");
+		for (var i = 0; i <= selectedQuotes.length - 1; i++){
+			$('#selected').append("<li>" + selectedQuotes[i] + "</li>");
+		}
+	} else {
+		clearOptions();
+	}
+}
+
+function clearOptions(){
+	selectedQuotes = [];
+	$('#selected').html("");
+}
+
+$(function(){ //Same as $document).ready(function(){
+	var quotesLength = quotes.length;
+	var n = 0;
+
+	$('#next').click(function() {
+		if (n < quotesLength-1){
+			n++;
+			printQuote(n);
+		} else{
+			n = 0;
+			printQuote(n);
+		};
+	});
+
+	$('#back').click(function() {
+		if (n > 0){
+			n--;
+			printQuote(n);
+		// } else if ( n === 0 ){
+		// 	printQuote(0);
+		} else {
+			n = 78;
+			printQuote(n);
+		}
+	});
+
+	$('#select').click(function() {
+		selectedQuote(n);
+	});
+
+	$('#unselect').click(function() {
+		if (selectedQuotes.length != 0){
+		unselectQuote();
+		};
+	});
+
+	$('#clearall').click(function() {
+		clearOptions();
+	});
+
+});
+
+
+
+
